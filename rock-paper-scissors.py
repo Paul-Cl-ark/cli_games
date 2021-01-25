e2a6aab00c29e5c42a9512b05292b2f315cbe47d
Paul Clark <clark.paul123@gmail.com>
2021-01-25 19:27:10 +0000

    basic game logic for rock paper scissors

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -13,6 +13,17 @@ from random import choice
 moves = ('rock', 'paper', 'scissors')
 
 
+def calculate_winner(computer_move, player_move):
+    if computer_move == player_move:
+        return 'draw'
+    elif computer_move == 'rock' and player_move == 'scissors' \
+            or computer_move == 'scissors' and player_move == 'paper' \
+            or computer_move == 'paper' and player_move == 'rock':
+        return 'computer'
+    else:
+        return 'player'
+
+
 def get_symbol(input):
     symbol_dictionary = {
         'rock': '👊',
@@ -32,6 +43,9 @@ def play():
     player_move = input('\nEnter \'rock\', \'paper\' or \'scissors\'... ')
 
     print_moves(computer_move, player_move)
+    winner = calculate_winner(computer_move, player_move)
+
+    print(winner)
 
 
 play()
